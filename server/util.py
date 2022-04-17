39e85b5d9909500d971c1a3723a4a5b90cb5425a
Sinchan <sinchan.tex@gmail.com>
2022-04-17 12:53:20 +0530

    bangalore house prediction site file

---
+++ server/util.py
@@ -0,0 +1,51 @@
+import json
+import pickle
+import numpy as np
+
+__locations = None
+__data_columns = None
+__model = None
+
+
+def get_estimated_price(location, sqft, bhk, bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0], 2)
+
+
+def get_location_names():
+    global __locations
+    return __locations
+
+
+def load_saved_artifacts():
+    print('loading saved artifacts...start')
+    global __data_columns
+    global __locations
+    global __model
+    with open('./artifacts/columns.json', 'r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
+
+    with open('./artifacts/bhp_model.pickle', 'rb') as f:
+        __model = pickle.load(f)
+    print('loading saved artifacts...done')
+
+
+if __name__ == "main":
+    load_saved_artifacts()
+    print(get_location_names())
+    print(get_estimated_price('1st phase jp nagar', 1000, 3, 3))
+    print(get_estimated_price('1st phase jp nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2))
+    print(get_estimated_price('Ejipura', 1000, 2, 2))
